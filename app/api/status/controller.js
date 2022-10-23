515bc4fb46657450daa438a70acb669bb270eb37
EricBollar <esbollar@gmail.com>
2022-10-23 10:36:15 -0400

    working GET dev/status

--- app/api/status/controller.js
+++ app/api/status/controller.js
@@ -1,5 +1,24 @@
-exports.currentStatus = function (req, res) {
-  res.status(200).send({
-    status: 'OK'
+let db = require('app/lib/mongodb');
+
+exports.currentStatus = async function (req, res) {
+  // check connection to database
+  if (db.readyState === 1) {
+    res.status(200).send({
+      status: 'OK'
+    })
+    return;
+  }
+
+  // get current state
+  let response = "FAILED TO CONNECT";
+  switch(db.readyState) {
+    case 0: response = "DISCONNECTED"; break;
+    case 2: response = "CONNECTING"; break;
+    case 3: response = "DISCONNECTING"; break;
+  };
+
+  // throw error
+  res.status(500).send({
+    status: response
   })
 }
